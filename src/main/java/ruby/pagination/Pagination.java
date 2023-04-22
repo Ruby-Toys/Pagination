c4bd1bc3572b22faad6b64e80390da079dc567e0
MisterRuby <ruby8700@naver.com>
2023-04-23 01:47:30 +0900

    feat : JPA 의존성 제거

--- src/main/java/ruby/pagination/Pagination.java
+++ src/main/java/ruby/pagination/Pagination.java
@@ -1,7 +1,6 @@
 package ruby.pagination;
 
 import lombok.Getter;
-import org.springframework.data.domain.Page;
 
 @Getter
 public class Pagination {
@@ -10,13 +9,6 @@ public class Pagination {
     private final int totalPages;
     private final int size;
     private final long totalElements;
-
-    public <T> Pagination(Page<T> page) {
-        this.number = page.getNumber();
-        this.totalPages = page.getTotalPages();
-        this.size = page.getSize();
-        this.totalElements = page.getTotalElements();
-    }
 
     public Pagination(int number, int totalCount, int size) {
         this.number = number;
--- src/test/java/ruby/pagination/TestVO.java
+++ src/test/java/ruby/pagination/TestVO.java
@@ -1,7 +1,6 @@
 package ruby.pagination;
 
 import lombok.Data;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -10,11 +9,6 @@ public class TestVO {
 
     private List<String> dataList;
     private Pagination pagination;
-
-    public TestVO(Page<String> page) {
-        this.dataList = page.getContent();
-        this.pagination = new Pagination(page);
-    }
 
     public TestVO(List<String> dataList, int number, int totalCount, int size) {
         this.dataList = dataList;
